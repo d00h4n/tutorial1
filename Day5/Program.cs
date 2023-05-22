825979b8bed9b160272ed4390af91db8674744db
d00h4n <108811042+d00h4n@users.noreply.github.com>
2023-05-23 05:35:31 +0700

    Update Program.cs

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,48 +1,48 @@
-﻿// // class Cat {
-// //     public Leg totalLeg;
-// //     public void SetLeg(Leg x)
-// //     {
-// //         totalLeg = x;
-// //     }
-// //     public int GetTotalLeg()
-// //     {
-// //         return totalLeg.LegSize;
-// //     }
-// // }
-// // class Leg
-// // {
-// //     public int LegSize;
-// //     public Nail totalNail;
-// //     public Leg(Nail x)
-// //     {
-// //         totalNail = x;
-// //         LegSize = 4;
-// //     }
-// // }
-// // class Nail 
-// // {
-// //     public int NailLength;
-// //     public Nail()
-// //     {
-// //         NailLength = 4;
-// //     }
-// // }
-// // static class Program
-// // {
-// //     static void Main()
-// //     {
-// //         Cat cotton = new Cat();
-// //         Nail catNail = new Nail();
-// //         Leg catLeg = new Leg(catNail);
+class Cat {
+     public Leg totalLeg;
+     public void SetLeg(Leg x)
+     {
+         totalLeg = x;
+     }
+     public int GetTotalLeg()
+     {
+         return totalLeg.LegSize;
+     }
+ }
+ class Leg
+ {
+     public int LegSize;
+     public Nail totalNail;
+     public Leg(Nail x)
+     {
+         totalNail = x;
+         LegSize = 4;
+     }
+ }
+class Nail 
+{
+     public int NailLength;
+    public Nail()
+    {
+        NailLength = 4;
+    }
+}
+static class Program
+{
+   static void Main()
+    {
+        Cat cotton = new Cat();
+        Nail catNail = new Nail();
+        Leg catLeg = new Leg(catNail);
         
-// //         cotton.SetLeg(catLeg);
+        cotton.SetLeg(catLeg);
         
-// //         cotton.totalLeg = catLeg;
+       cotton.totalLeg = catLeg;
         
-// //         Console.WriteLine(cotton.GetTotalLeg());
-// //         Console.WriteLine(cotton.totalLeg.totalNail.NailLength);
-// //     }
-// // }
+        Console.WriteLine(cotton.GetTotalLeg());
+        Console.WriteLine(cotton.totalLeg.totalNail.NailLength);
+    }
+}
 
 
 // // TUPLE
