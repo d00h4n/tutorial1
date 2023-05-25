690b7c25735a7f36f2aa8787ed21fb584cccac4a
d00h4n <doohan.mintarso@gmail.com>
2023-05-25 13:16:26 +0700

    Update no //

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -43,124 +43,124 @@
         }
     }
 
-// public delegate void PInventory();
+public delegate void PInventory();
 
-// class Program {
-//     static void Main(string[] args) {
-//         PInventory equipment1 = new PInventory(Use.eq1);
-//         PInventory equipment2 = new PInventory(Use.eq2);
-//         PInventory equipment3 = new PInventory(Use.eq3);
-//         PInventory equipment4 = new PInventory(Use.eq4);
-//         PInventory equipment5 = new PInventory(Use.eq5);
-//         PInventory equipment6 = new PInventory(Use.eq6);
-//         string retry = "Done";
-//         Console.WriteLine("Use 1-6 your equipment:");
-//         do{
+class Program {
+    static void Main(string[] args) {
+        PInventory equipment1 = new PInventory(Use.eq1);
+        PInventory equipment2 = new PInventory(Use.eq2);
+        PInventory equipment3 = new PInventory(Use.eq3);
+        PInventory equipment4 = new PInventory(Use.eq4);
+        PInventory equipment5 = new PInventory(Use.eq5);
+        PInventory equipment6 = new PInventory(Use.eq6);
+        string retry = "Done";
+        Console.WriteLine("Use 1-6 your equipment:");
+        do{
 
         
-//         char input = Char.ToUpper(Console.ReadKey().KeyChar);
-//         Console.WriteLine();
+        char input = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
 
-//         switch (input++) {
-//             case '1':
-//                 PerformAction(equipment1);
-//                 break;
-//             case '2':
-//                 PerformAction(equipment2);
-//                 break;
-//             case '3':
-//                 PerformAction(equipment3);
-//                 break;
-//             case '4':
-//                 PerformAction(equipment4);
-//                 break;
-//             case '5':
-//                 PerformAction(equipment5);
-//                 break;
-//             case '6':
-//                 PerformAction(equipment6);
-//                 break;
-//             default:
-//                 Console.WriteLine("Not in the list");
-//                 Console.WriteLine("Would you like to retry?");
-//                 retry = Console.ReadLine();
-//                 break;
+        switch (input++) {
+            case '1':
+                PerformAction(equipment1);
+                break;
+            case '2':
+                PerformAction(equipment2);
+                break;
+            case '3':
+                PerformAction(equipment3);
+                break;
+            case '4':
+                PerformAction(equipment4);
+                break;
+            case '5':
+                PerformAction(equipment5);
+                break;
+            case '6':
+                PerformAction(equipment6);
+                break;
+            default:
+                Console.WriteLine("Not in the list");
+                Console.WriteLine("Would you like to retry?");
+                retry = Console.ReadLine();
+                break;
             
-//         }
-//         }
-//         while  (retry != "No");
+        }
+        }
+        while  (retry != "No");
         
         
-//     }
+    }
 
-//     static void PerformAction(PInventory Use) {
-//         Use.Invoke();
-//     }
-// }
-
-
-// public class Use {
-//     // Player action methods
-//     public static void eq1() {
-//         Console.WriteLine("Switch to Rifle.");
-//     }
-
-//     public static void eq2() {
-//         Console.WriteLine("Switch to HandGun");
-//     }
-
-//     public static void eq3() {
-//         Console.WriteLine("Switch to Brick");
-//     }
-//     public static void eq4() {
-//         Console.WriteLine("Switch to Coin");
-//     }
-//     public static void eq5() {
-//         Console.WriteLine("Switch to Syringe");
-//     }
-//     public static void eq6() {
-//         Console.WriteLine("Switch to hands");
-//     }
-// }
-
-// public class Publisher
-// {
-//     // B1_Event untuk mengirim pesan ke subscriber
-//     public event EventHandler<string> MessageSent;
-
-//     public void SendMessage(string message)
-//     {
-//         // Memanggil semua metode yang terdaftar pada event
-//         MessageSent?.Invoke(this, message);
-//     }
-// }
-
-// public class Subscriber
-// {
-//     public void OnMessageReceived(object sender, string message)
-//     {   
-
-//         Console.WriteLine($"Subscriber menerima pesan: {message}");
-//     }
-// }
-
-// public class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         Publisher publisher = new Publisher();
-//         Subscriber subscriber1 = new Subscriber();
-//         Subscriber subscriber2 = new Subscriber();
-//         Subscriber subscriber3 = new Subscriber();
+    static void PerformAction(PInventory Use) {
+        Use.Invoke();
+    }
+}
 
 
-//         // Mendaftarkan metode subscriber ke event
-//         publisher.MessageSent += subscriber1.OnMessageReceived;
-//         publisher.MessageSent += subscriber2.OnMessageReceived;
+public class Use {
+    // Player action methods
+    public static void eq1() {
+        Console.WriteLine("Switch to Rifle.");
+    }
+
+    public static void eq2() {
+        Console.WriteLine("Switch to HandGun");
+    }
+
+    public static void eq3() {
+        Console.WriteLine("Switch to Brick");
+    }
+    public static void eq4() {
+        Console.WriteLine("Switch to Coin");
+    }
+    public static void eq5() {
+        Console.WriteLine("Switch to Syringe");
+    }
+    public static void eq6() {
+        Console.WriteLine("Switch to hands");
+    }
+}
+
+public class Publisher
+{
+    // B1_Event untuk mengirim pesan ke subscriber
+    public event EventHandler<string> MessageSent;
+
+    public void SendMessage(string message)
+    {
+        // Memanggil semua metode yang terdaftar pada event
+        MessageSent?.Invoke(this, message);
+    }
+}
+
+public class Subscriber
+{
+    public void OnMessageReceived(object sender, string message)
+    {   
+
+        Console.WriteLine($"Subscriber menerima pesan: {message}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Publisher publisher = new Publisher();
+        Subscriber subscriber1 = new Subscriber();
+        Subscriber subscriber2 = new Subscriber();
+        Subscriber subscriber3 = new Subscriber();
 
 
-//         publisher.SendMessage("Halo, ini pesan untuk semua subscriber!");
-//     }
-// }
+        // Mendaftarkan metode subscriber ke event
+        publisher.MessageSent += subscriber1.OnMessageReceived;
+        publisher.MessageSent += subscriber2.OnMessageReceived;
+
+
+        publisher.SendMessage("Halo, ini pesan untuk semua subscriber!");
+    }
+}
 
 }
